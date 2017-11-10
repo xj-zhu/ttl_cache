585da8330477f671333ba4b423c1bab6ad388c3c
zhuxingjian <jianzxz@sina.com>
2017-11-10 14:05:56 +0800

    core and test

---
+++ core/ttl_cache.h
@@ -0,0 +1,574 @@
+#ifndef _TTL_CACHE_H_
+#define _TTL_CACHE_H_
+
+#include <memory>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+#include <functional>
+#include <list>
+#include <map>
+#include <algorithm>
+#include <type_traits>
+#include <assert.h>
+
+namespace ttl
+{
+	namespace type_traits
+	{
+		template<typename _Tp, typename _Up>
+		class _is_appendable_helper
+		{
+			template<typename _Tp1, typename _Up1,
+				typename = decltype(std::declval<_Tp1>() += std::declval<_Up1>())>
+				static std::true_type __test(int);
+
+			template<typename, typename>
+			static std::false_type __test(...);
+
+		public:
+			typedef decltype(__test<_Tp&, _Up&>(0)) type;
+		};
+
+		template<typename _Tp, typename _Up>
+		struct is_appendable : public _is_appendable_helper<_Tp, _Up>::type
+		{ };
+	}
+
+	using namespace std::chrono;
+
+	enum DataStoreType	//数据暂存方式
+	{
+		DS_Err,			//错误
+		DS_SINGLE,		//单条
+		DS_QUEUE,		//序列
+	};
+
+	typedef int DataType;
+
+	static constexpr DataType DT_Err = -1;
+
+	class cache_mgr;
+
+	class cache_base
+	{
+	public:
+		virtual ~cache_base() {}
+		virtual long use_count() const noexcept
+		{
+			assert(false);
+			return 0;
+		}
+
+	private:
+		virtual void _OnCopy() {};
+		virtual void _OnAppend(const cache_base& that) {};
+
+	public:
+		cache_base& operator = (const cache_base& that)
+		{
+			if (this != &that)
+			{
+				m_managed = that.m_managed;
+				_OnCopy();
+			}
+			return *this;
+		}
+		cache_base& operator+=(const cache_base& that)
+		{
+			_OnAppend(that);
+			return *this;
+		}
+
+	public:
+		cache_base*	m_managed = nullptr;
+
+		friend class cache_mgr;
+	};
+
+	class cache_mgr
+	{
+		enum TtlStrategy
+		{
+			TTL_WHEN_START,
+			TTL_WHEN_ALL_RELEASE,
+		};
+	public:
+		static cache_mgr& Instance() { static cache_mgr inst; return inst; }
+
+	public:
+		template <typename... Args>
+		bool GetCache(cache_base&cache, DataType edt, Args&&... args)
+		{
+			typedef std::map<std::tuple<Args...>, std::weak_ptr<cache_base>> CacheMap;
+
+			std::lock_guard<std::recursive_mutex> l(m_mutex);
+			auto it1 = m_records.find(edt);
+			if (it1 != m_records.end())
+			{
+				CacheMap* pmap = (CacheMap*)(it1->second);
+				if (pmap)
+				{
+					auto it2 = pmap->find(std::forward_as_tuple(std::forward<Args>(args)...));
+					if (it2 != pmap->end())
+					{
+						auto shared = it2->second.lock();
+						if (shared && shared.get())
+						{
+							cache = *(shared.get());
+							return true;
+						}
+						pmap->erase(it2);
+						if (pmap->empty())
+						{
+							delete pmap;
+							m_records.erase(edt);
+						}
+					}
+				}
+			}
+			return false;
+		}
+
+		template <typename... Args>
+		void SetCache(cache_base* cache, DataStoreType edst, DataType edt, time_t lifems, Args&&... args)
+		{
+			typedef std::map<std::tuple<Args...>, std::weak_ptr<cache_base>> CacheMap;
+
+			std::shared_ptr<cache_base> shared(cache);
+			std::lock_guard<std::recursive_mutex> l(m_mutex);
+			auto it = m_records.find(edt);
+			CacheMap* pmap = nullptr;
+			if (it != m_records.end() && it->second)
+			{
+				pmap = (CacheMap*)(it->second);
+			}
+			else
+			{
+				pmap = new CacheMap;
+				m_records[edt] = pmap;
+			}
+			if (pmap)
+			{
+				auto& ptr = (*pmap)[std::forward_as_tuple(std::forward<Args>(args)...)];
+				switch (edst)
+				{
+				default:
+					assert(false);
+					break;
+				case DS_QUEUE:
+					if (ptr.expired())
+						ptr = shared;
+					else
+					{
+						*(ptr.lock().get()) += *(shared.get());
+						shared.reset();
+					}
+					break;
+				case DS_SINGLE:
+					m_caches.erase(ptr.lock());
+					ptr = shared;
+					break;
+				}
+				if (shared)
+					m_caches.insert(std::make_pair(std::move(shared), lifems));
+				if (_CheckStrategy(TTL_WHEN_START))
+					_StartTTL(cache);
+			}
+		}
+
+		template <typename... Args>
+		void ClrCache(DataType edt, Args&&... args)
+		{
+			typedef std::map<std::tuple<Args...>, std::weak_ptr<cache_base>> CacheMap;
+
+			std::lock_guard<std::recursive_mutex> l(m_mutex);
+			auto it = m_records.find(edt);
+			if (it != m_records.end() && it->second)
+			{
+				CacheMap* pmap = (CacheMap*)(it->second);
+				if (pmap)
+				{
+					auto tp = std::forward_as_tuple(std::forward<Args>(args)...);
+					auto itf = pmap->find(tp);
+					if (itf != pmap->end())
+					{
+						m_caches.erase(itf->second.lock());
+						pmap->erase(itf);
+					}
+					if (pmap->empty())
+					{
+						delete pmap;
+						m_records.erase(edt);
+					}
+				}
+			}
+		}
+
+		void StartTTL(cache_base* cache)
+		{
+			if (_CheckStrategy(TTL_WHEN_START))
+				return;
+			std::lock_guard<std::recursive_mutex> l(m_mutex);
+			_StartTTL(cache);
+		}
+
+		void StopTTL(cache_base* cache) {/*donothing.*/ }
+
+	private:
+		void _ThreadLoop()
+		{
+			while (1)
+			{
+				std::unique_lock<std::recursive_mutex> l(m_mutex);
+				m_condvar.wait_for(l, m_perchackduration);
+				for (auto it = m_queue.begin(); it != m_queue.end();)
+				{
+					if ((!m_loop_running) || (it->first <= std::chrono::steady_clock::now()))
+					{
+						if ((_CheckStrategy(TTL_WHEN_ALL_RELEASE)) ? (it->second && it->second->use_count() == 1) : true)
+							m_caches.erase(it->second);
+						it = m_queue.erase(it);
+					}
+					else if ((m_loop_running) || false)
+						break;
+				}
+				if (!m_loop_running)
+					break;
+			}
+		}
+
+		void _StartTTL(cache_base* cache)
+		{
+			std::shared_ptr<cache_base> shared;
+			auto it = std::find_if(m_caches.begin()
+				, m_caches.end()
+				, [&shared, cache](std::pair<const std::shared_ptr<cache_base>, time_t>& pr)
+			{
+				if (pr.first.get() == cache)
+				{
+					shared = pr.first;
+					return true;
+				}
+				return false;
+			});
+			if (it != m_caches.end())
+			{
+				if (time_t(-1) != it->second)
+					m_queue.insert(std::make_pair(std::chrono::steady_clock::now()
+						+ std::chrono::duration_cast<std::chrono::steady_clock::duration>(std::chrono::milliseconds(it->second))
+						, std::move(shared)));
+				m_condvar.notify_one();
+			}
+		}
+
+		bool _CheckStrategy(TtlStrategy strategy)
+		{
+			return (m_strategy == TTL_WHEN_START)
+				? (strategy == TTL_WHEN_START)
+				: (strategy == TTL_WHEN_ALL_RELEASE);
+		}
+
+	private:
+		TtlStrategy m_strategy = TTL_WHEN_START;
+		std::chrono::milliseconds m_perchackduration = 5000ms;
+		bool m_loop_running = true;
+		std::thread* m_thread = nullptr;
+
+		std::multimap<std::chrono::steady_clock::time_point, std::shared_ptr<cache_base>> m_queue;
+		std::map<std::shared_ptr<cache_base>, time_t> m_caches;
+		std::map<DataType, void*> m_records;
+		std::recursive_mutex m_mutex;
+		std::condition_variable_any m_condvar;
+
+	private:
+		cache_mgr()
+		{
+			m_thread = new std::thread(std::bind(&cache_mgr::_ThreadLoop, this));
+		}
+		~cache_mgr()
+		{
+			m_loop_running = false;
+			m_condvar.notify_one();
+			if (m_thread)
+				m_thread->join();
+			delete m_thread;
+			m_thread = nullptr;
+			std::for_each(m_records.begin()
+				, m_records.end()
+				, [](std::pair<const DataType, void*>& pr) {delete pr.second; }
+			);
+		}
+		cache_mgr(const cache_mgr& that) = delete;
+		cache_mgr(cache_mgr&& that) = delete;
+		cache_mgr& operator=(const cache_mgr& that) = delete;
+		cache_mgr& operator=(cache_mgr&& that) = delete;
+	};
+
+	template <typename _Ty>
+	class cache : public cache_base
+	{
+		typedef cache<_Ty> _Myt;
+
+	private:
+		template <typename... Args>
+		void _CheckInConstructor(Args&&... _args)
+		{
+			if (use_count() == 1)
+			{
+				assert(nullptr == m_managed);
+				MangeTTL(std::forward<Args>(_args)...);
+			}
+			else/* if (use_count() == 2)*/
+			{
+				assert(nullptr != m_managed);
+				StopTTL();
+			}
+		}
+		void _CheckInDestructor()
+		{
+			if (use_count() == 2)
+			{
+				StartTTL();
+			}
+		}
+		void _OnCopy()
+		{
+			if (_CopyCache(this, dynamic_cast<_Myt*>(m_managed)))
+			{
+				_CheckInConstructor();
+			}
+		}
+		void _OnAppend(const cache_base& that)
+		{
+			__OnAppend(dynamic_cast<_Myt*>(that.m_managed), type_traits::is_appendable<_Ty, _Ty>());
+		}
+		void __OnAppend(const _Myt* that, std::true_type&&)
+		{
+			if (that)
+			{
+				if (m_shared && that->m_shared)
+				{
+					*m_shared += *(that->m_shared);
+				}
+			}
+		}
+		void __OnAppend(...)
+		{
+			assert(false);
+		}
+		bool _CopyCache(cache<_Ty>* const dst, const cache<_Ty>* const src)
+		{
+			if (!dst || !src)
+				return false;
+			dst->m_shared = src->m_shared;
+			dst->m_Edst = src->m_Edst;
+			dst->m_Edt = src->m_Edt;
+			dst->m_lifeMs = src->m_lifeMs;
+			return true;
+		}
+
+	private:
+		template <typename... Args>
+		void MangeTTL(Args&&... _args)
+		{
+			m_managed = new cache<_Ty>;
+			m_managed->m_managed = m_managed;
+			_CopyCache(dynamic_cast<_Myt*>(m_managed), this);
+			cache_mgr::Instance().SetCache(m_managed, m_Edst, m_Edt, m_lifeMs, std::forward<Args>(_args)...);
+		}
+
+		void StartTTL()
+		{
+			cache_mgr::Instance().StartTTL(m_managed);
+		}
+
+		void StopTTL()
+		{
+			cache_mgr::Instance().StopTTL(m_managed);
+		}
+
+	public:
+		cache() noexcept
+		{	// construct empty cache
+		}
+
+		template<class _Ux,
+			typename... Args>
+			explicit cache(_Ux *_Px, DataStoreType _Edst, DataType _Edt, time_t _lifeMs, Args&&... _args)
+			: m_shared(_Px)
+			, m_Edst(_Edst)
+			, m_Edt(_Edt)
+			, m_lifeMs(_lifeMs)
+		{	// construct cache object that owns _Px
+			_CheckInConstructor(std::forward<Args>(_args)...);
+		}
+
+		template<class _Ux,
+			class _Dx,
+			typename... Args>
+			cache(_Ux *_Px, _Dx _Dt, DataStoreType _Edst, DataType _Edt, time_t _lifeMs, Args&&... _args)
+			: m_shared(_Px, _Dt)
+			, m_Edst(_Edst)
+			, m_Edt(_Edt)
+			, m_lifeMs(_lifeMs)
+		{	// construct with _Px, deleter
+			_CheckInConstructor(std::forward<Args>(_args)...);
+		}
+
+		cache(nullptr_t) noexcept
+		{	// construct empty cache
+		}
+
+		template<class _Dx,
+			typename... Args>
+			cache(nullptr_t _N, _Dx _Dt, DataStoreType _Edst, DataType _Edt, time_t _lifeMs, Args&&... _args)
+			: m_shared(_N, _Dt)
+			, m_Edst(_Edst)
+			, m_Edt(_Edt)
+			, m_lifeMs(_lifeMs)
+		{	// construct with nullptr, deleter
+			_CheckInConstructor(std::forward<Args>(_args)...);
+		}
+
+		template<class _Dx,
+			class _Alloc,
+			typename... Args>
+			cache(nullptr_t _N, _Dx _Dt, _Alloc _Ax, DataStoreType _Edst, DataType _Edt, time_t _lifeMs, Args&&... _args)
+			: m_shared(_N, _Dt, _Ax)
+			, m_Edst(_Edst)
+			, m_Edt(_Edt)
+			, m_lifeMs(_lifeMs)
+		{	// construct with nullptr, deleter, allocator
+			_CheckInConstructor(std::forward<Args>(_args)...);
+		}
+
+		template<class _Ux,
+			class _Dx,
+			class _Alloc,
+			typename... Args>
+			cache(_Ux *_Px, _Dx _Dt, _Alloc _Ax, DataStoreType _Edst, DataType _Edt, time_t _lifeMs, Args&&... _args)
+			: m_shared(_Px, _Dt, _Ax)
+			, m_Edst(_Edst)
+			, m_Edt(_Edt)
+			, m_lifeMs(_lifeMs)
+		{	// construct with _Px, deleter, allocator
+			_CheckInConstructor(std::forward<Args>(_args)...);
+		}
+
+		template<class _Ty2>
+		cache(const cache<_Ty2>& _Right, _Ty *_Px) noexcept : m_shared(_Right.m_shared, _Px)
+		{	// construct cache object that aliases _Right
+			m_managed = _Right.m_managed;
+			m_Edst = _Right.m_Edst;
+			m_Edt = _Right.m_Edt;
+			m_lifeMs = _Right.m_lifeMs;
+			_CheckInConstructor();
+		}
+
+		cache(const _Myt& _Other) noexcept : m_shared(_Other.m_shared)
+		{	// construct cache object that owns same resource as _Other
+			m_managed = _Other.m_managed;
+			m_Edst = _Other.m_Edst;
+			m_Edt = _Other.m_Edt;
+			m_lifeMs = _Other.m_lifeMs;
+			_CheckInConstructor();
+		}
+
+		template<class _Ty2,
+			class = typename std::enable_if<std::is_convertible<_Ty2 *, _Ty *>::value,
+			void>::type>
+			cache(const cache<_Ty2>& _Other) noexcept : m_shared(_Other.m_shared)
+		{	// construct cache object that owns same resource as _Other
+			m_managed = _Other.m_managed;
+			m_Edst = _Other.m_Edst;
+			m_Edt = _Other.m_Edt;
+			m_lifeMs = _Other.m_lifeMs;
+			_CheckInConstructor();
+		}
+		_Myt& operator=(_Myt&& _Right) noexcept
+		{	// take resource from _Right
+			cache(std::move(_Right)).swap(*this);
+			return (*this);
+		}
+
+		template<class _Ty2>
+		_Myt& operator=(cache<_Ty2>&& _Right) noexcept
+		{	// take resource from _Right
+			cache(std::move(_Right)).swap(*this);
+			return (*this);
+		}
+
+		_Myt& operator=(const _Myt& _Right) noexcept
+		{	// assign shared ownership of resource owned by _Right
+			cache(_Right).swap(*this);
+			return (*this);
+		}
+
+		template<class _Ty2>
+		_Myt& operator=(const cache<_Ty2>& _Right) noexcept
+		{	// assign shared ownership of resource owned by _Right
+			cache(_Right).swap(*this);
+			return (*this);
+		}
+
+		cache(_Myt&& _Right) noexcept
+			: cache_base(std::move(_Right))
+			, m_shared(std::move(_Right.m_shared))
+			, m_Edst(std::move(_Right.m_Edst))
+			, m_Edt(std::move(_Right.m_Edt))
+			, m_lifeMs(std::move(_Right.m_lifeMs))
+		{	// construct cache object that takes resource from _Right
+		}
+
+		void swap(_Myt& _Other) noexcept
+		{	// swap pointers
+			m_shared.swap(_Other.m_shared);
+			std::swap(m_managed, _Other.m_managed);
+			std::swap(m_Edst, _Other.m_Edst);
+			std::swap(m_Edt, _Other.m_Edt);
+			std::swap(m_lifeMs, _Other.m_lifeMs);
+		}
+
+		_Ty *get() const noexcept
+		{	// return pointer to resource
+			return (m_shared.get());
+		}
+
+		long use_count() const noexcept
+		{	// return use count
+			return m_shared.use_count();
+		}
+
+		typename std::add_lvalue_reference<_Ty>::type operator*() const noexcept
+		{	// return reference to resource
+			return (*(m_shared.get()));
+		}
+
+		_Ty *operator->() const noexcept
+		{	// return pointer to resource
+			return (m_shared.get());
+		}
+
+		~cache() noexcept
+		{	// release resource
+			_CheckInDestructor();
+		}
+
+	private:
+		std::shared_ptr<_Ty> m_shared;
+		DataStoreType m_Edst = DS_Err;
+		DataType m_Edt = DT_Err;
+		time_t m_lifeMs = 60000;
+	};
+}
+
+template<class _Ty>
+void swap(ttl::cache<_Ty>& _Left,
+	ttl::cache<_Ty>& _Right) noexcept
+{	// swap _Left and _Right shared_ptrs
+	_Left.swap(_Right);
+}
+
+#endif // _TTL_CACHE_H_
---
+++ test/test_ttlcache.cpp
@@ -0,0 +1,218 @@
+#include "ttl_cache.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std::chrono;
+using namespace std::string_literals;
+
+enum EmDataType : ttl::DataType	//数据类型
+{
+	DT_Err = ttl::DT_Err,	//错误
+	DT_AccountInfo,
+	DT_AccountList,
+	DT_TradeHistory,
+};
+
+class AccountInfo
+{
+public:
+	AccountInfo(long id, const std::string& owner, const std::string& bank, unsigned long balance)
+		: m_id(id)
+		, m_owner(owner)
+		, m_bank(bank)
+		, m_balance(balance)
+	{
+		std::cout << "AccountInfo standard constructor." << std::endl;
+	}
+	AccountInfo(const AccountInfo& right)
+	{
+		m_id = (right.m_id);
+		m_owner = (right.m_owner);
+		m_bank = (right.m_bank);
+		m_balance = (right.m_balance);
+		std::cout << "AccountInfo copy constructor." << std::endl;
+	}
+	AccountInfo(AccountInfo&& right)
+	{
+		m_id = std::move(right.m_id);
+		m_owner = std::move(right.m_owner);
+		m_bank = std::move(right.m_bank);
+		m_balance = std::move(right.m_balance);
+		std::cout << "AccountInfo move constructor." << std::endl;
+	}
+	~AccountInfo()
+	{
+		std::cout << "AccountInfo destructor." << std::endl;
+	}
+
+public:
+	long m_id;
+	std::string m_owner;
+	std::string m_bank;
+	unsigned long m_balance;
+};
+
+//使用场景下，不需要保持right不变（const），so，使用此种写法性能更高
+std::list<AccountInfo>& operator+=(std::list<AccountInfo>& left, std::list<AccountInfo>& right)
+{
+	for(auto it = right.begin(); it != right.end(); ++it)
+		left.push_back(std::move(*it));
+	return left;
+}
+
+class TradeHistory
+{
+public:
+	TradeHistory(long account)
+		: m_account(account)
+	{
+		std::cout << "TradeHistory standard constructor." << std::endl;
+	}
+	TradeHistory(const TradeHistory& right)
+	{
+		m_account = (right.m_account);
+		m_history = (right.m_history);
+		std::cout << "TradeHistory copy constructor." << std::endl;
+	}
+	TradeHistory(TradeHistory&& right)
+	{
+		m_account = std::move(right.m_account);
+		m_history = std::move(right.m_history);
+		std::cout << "TradeHistory move constructor." << std::endl;
+	}
+	~TradeHistory()
+	{
+		std::cout << "TradeHistory destructor." << std::endl;
+	}
+	TradeHistory& operator+=(TradeHistory& that)
+	{
+		if (m_account == that.m_account)
+			for (auto it = that.m_history.begin(); it != that.m_history.end(); ++it)
+				m_history.push_back(std::move(*it));
+		return *this;
+	}
+
+public:
+	long m_account;
+	std::list<std::pair<long, std::string>> m_history;	//list<收支金额(+收入/-支出)，金额变动说明>
+};
+
+void testttlcache_1()//单条数据
+{
+	//1. 没有缓存时获取失败
+	ttl::cache<AccountInfo> s1;
+	//约定后两个参数表示取【持有人为"zxj"的账号为"100101"的账户】的缓存数据
+	bool ret1 = ttl::cache_mgr::Instance().GetCache(s1, DT_AccountInfo, "zxj"s, 100101);
+	//2.1) 增加一个生命周期10s的缓存数据
+	{
+		ttl::cache<AccountInfo> c1(new AccountInfo(100101, "zxj", "中国银行", 666666), ttl::DS_SINGLE, DT_AccountInfo, 10000, "zxj"s, 100101);
+	}
+	//2.2) 即时离开了c1的作用域，一样能够获取到前面加入的缓存数据，【注意：后两个参数类型与增加缓存时对应】
+	ttl::cache<AccountInfo> s2;
+	bool ret2 = ttl::cache_mgr::Instance().GetCache(s2, DT_AccountInfo, "zxj"s, 100101);
+	//3.1) 无法获取到未加入的缓存数据，【注意：后两个参数类型与增加缓存时对应】
+	ttl::cache<AccountInfo> s3;
+	bool ret3 = ttl::cache_mgr::Instance().GetCache(s3, DT_AccountInfo, "lxy"s, 100102);
+	//3.2) 增加一个生命周期10s的缓存数据
+	ttl::cache<AccountInfo> c2(new AccountInfo(100102, "lxy", "建设银行", 654321), ttl::DS_SINGLE, DT_AccountInfo, 10000, "lxy"s, 100102);
+	//3.3) 这时可以获取到（对比3.1步骤）
+	ttl::cache<AccountInfo> s4;
+	bool ret4 = ttl::cache_mgr::Instance().GetCache(s4, DT_AccountInfo, "lxy"s, 100102);
+	//4.1) 修改了缓存中的账户余额
+	ttl::cache<AccountInfo> c3(new AccountInfo(100101, "zxj", "中国银行", 999999), ttl::DS_SINGLE, DT_AccountInfo, 10000, "zxj"s, 100101);
+	//4.2) 重新获取缓存数据，得到最新数据
+	ttl::cache<AccountInfo> s5;
+	bool ret5 = ttl::cache_mgr::Instance().GetCache(s5, DT_AccountInfo, "zxj"s, 100101);
+	//5.1) 清除【持有人为"zxj"的账号为"100101"的账户】的缓存数据
+	ttl::cache_mgr::Instance().ClrCache(DT_AccountInfo, "zxj"s, 100101);
+	//5.2) 获取不到对应的缓存了
+	ttl::cache<AccountInfo> s6;
+	bool ret6 = ttl::cache_mgr::Instance().GetCache(s6, DT_AccountInfo, "zxj"s, 100101);
+	//6. 使用和最开始调用不同的keys参数创建新缓存，会导致内存混乱，或致崩溃
+	ttl::cache<AccountInfo> c4(new AccountInfo(100101, "zxj", "中国银行", 888888), ttl::DS_SINGLE, DT_AccountInfo, 10000, "zxj"s, 100101,"中国银行"s);
+	ttl::cache<AccountInfo> s7;
+	bool ret7 = ttl::cache_mgr::Instance().GetCache(s7, DT_AccountInfo, "zxj"s, 100101, "中国银行"s);
+	//7. 等到缓存生命周期结束，则不能再获取到缓存数据【注意：具体能不能获取到和ttl::ttl_cache_mgr的缓存策略"ttl::ttl_cache_mgr::TtlStrategy"有关】
+	std::this_thread::sleep_for(std::chrono::seconds(10));
+	ttl::cache<AccountInfo> s8;
+	bool ret8 = ttl::cache_mgr::Instance().GetCache(s8, DT_AccountInfo, "lxy"s, 100102);
+}
+
+void testttlcache_2()
+{
+	//1. 没有缓存时获取失败
+	ttl::cache<std::list<AccountInfo>> s1;
+	// keys只有一个参数，表示获取【持有人为"zxj"的账户列表】缓存数据
+	bool ret1 = ttl::cache_mgr::Instance().GetCache(s1, DT_AccountList,"zxj"s);
+	//2.1) 增加一个缓存数据,序列数据涉及到实时更新，比如订阅推送数据，可指定生命周期为无限长(time_t(-1))
+	auto l1 = new std::list<AccountInfo>;
+	l1->emplace_back(100103, "zxj", "交通银行", 900000);
+	ttl::cache<std::list<AccountInfo>> c1(l1, ttl::DS_QUEUE, DT_AccountList, -1, "zxj"s);
+	//2.2) 获取前面加入的缓存数据
+	ttl::cache<std::list<AccountInfo>> s2;
+	bool ret2 = ttl::cache_mgr::Instance().GetCache(s2, DT_AccountList, "zxj"s);
+	//3.1) 增加两个缓存数据项
+	auto l2 = new std::list<AccountInfo>;
+	l2->emplace_back(100104, "zxj", "农业银行", 980000);
+	l2->emplace_back(100105, "zxj", "工商银行", 990000);
+	ttl::cache<std::list<AccountInfo>> c2(l2, ttl::DS_QUEUE, DT_AccountList, -1, "zxj"s);
+	//3.2) 获取前面加入的缓存数据,应该有三条数据
+	ttl::cache<std::list<AccountInfo>> s3;
+	bool ret3 = ttl::cache_mgr::Instance().GetCache(s3, DT_AccountList, "zxj"s);
+	//4. 清除前面加入的"zxj"名下的缓存
+	ttl::cache_mgr::Instance().ClrCache(DT_AccountList,"zxj"s);
+	//5.1) 增加一个新的缓存数据项
+	auto l3 = new std::list<AccountInfo>;
+	l3->emplace_back(100106, "zxj", "招商银行", 970000);
+	ttl::cache<std::list<AccountInfo>> c3(l3, ttl::DS_QUEUE, DT_AccountList, -1, "zxj"s);
+	//2.2) 获取前面加入的缓存数据,应该只有一条数据
+	ttl::cache<std::list<AccountInfo>> s4;
+	bool ret4 = ttl::cache_mgr::Instance().GetCache(s4, DT_AccountList, "zxj"s);
+}
+
+void testttlcache_3()
+{
+	//1. 没有缓存时获取失败
+	ttl::cache<TradeHistory> s1;
+	// keys只有一个参数，表示获取【账号为"100101"的账户的交易明细】缓存数据
+	bool ret1 = ttl::cache_mgr::Instance().GetCache(s1, DT_TradeHistory, 100101);
+	//2.1) 增加一个缓存数据,序列数据涉及到实时更新，比如订阅推送数据，可指定生命周期为无限长(time_t(-1))
+	auto t1 = new TradeHistory(100101);
+	t1->m_history.emplace_back(6666, "发工资啦");
+	t1->m_history.emplace_back(-1000, "楼下足疗店一掷千金");
+	ttl::cache<TradeHistory> c1(t1, ttl::DS_QUEUE, DT_TradeHistory, -1, 100101);
+	//2.2) 获取前面加入的缓存数据
+	ttl::cache<TradeHistory> s2;
+	bool ret2 = ttl::cache_mgr::Instance().GetCache(s2, DT_TradeHistory, 100101);
+	//3.1) 增加两个缓存数据项
+	auto t2 = new TradeHistory(100101);
+	t2->m_history.emplace_back(100000104, "突然多了这么多，银行系统bug了？");
+	t2->m_history.emplace_back(-100000000, "XX银行：sorry~，上一笔系清洁工手误。。");
+	ttl::cache<TradeHistory> c2(t2, ttl::DS_QUEUE, DT_TradeHistory, -1, 100101);
+	//3.2) 获取前面加入的缓存数据,应该有三条数据
+	ttl::cache<TradeHistory> s3;
+	bool ret3 = ttl::cache_mgr::Instance().GetCache(s3, DT_TradeHistory, 100101);
+	//4. 清除前面加入的"100101账户"名下的缓存
+	ttl::cache_mgr::Instance().ClrCache(DT_TradeHistory, 100101);
+	//5.1) 增加一个新的缓存数据项
+	auto t3 = new TradeHistory(100101);
+	t3->m_history.emplace_back(-1314520, "emmm,光棍节发红包支出");
+	ttl::cache<TradeHistory> c3(t3, ttl::DS_QUEUE, DT_TradeHistory, -1, 100101);
+	//2.2) 获取前面加入的缓存数据,应该只有一条数据
+	ttl::cache<TradeHistory> s4;
+	bool ret4 = ttl::cache_mgr::Instance().GetCache(s4, DT_TradeHistory, 100101);
+}
+
+void test_ttlcache()
+{
+	testttlcache_1();
+	testttlcache_2();
+	testttlcache_3();
+}
+
+int main()
+{
+	test_ttlcache();
+	return 1;
+}
